fc8962cc9317ebc77fda381827c84378b17c0627
Thimmaraju <thimmaraju418@gmail.com>
2023-09-29 16:54:49 +0530

    JS programs

--- cypress/pageobjects/ex.js
+++ cypress/pageobjects/ex.js
@@ -144,3 +144,58 @@
 // console.log(removeduplicharacter("Thimmaraju"))
 //=========================================================
 
+// array1 = ["a","b","c", "d", "e"]
+
+// array2 =["1","2"]
+
+// output = ["a","1","b","2","c","d", "e"]
+
+
+//or
+
+// array1 = ["a","b","c", "d", "e"]
+
+// array2 =["1","2","3","4","5", "6","7"]
+
+// output = ["a","1","b","2","c","3",d","4", "e","5","6","7"]
+
+// var arr1len = array1.length
+
+// var arr2len = array2.length
+
+// var output = []
+
+// var len = array1.length > array2.length ? array1.length : array2.length
+
+// console.log(len)
+
+// for (let i = 0; i < len; i++) {
+
+//     if (i < array1.length) {
+
+//         output.push(array1[i])
+
+//     }
+
+//     if (i < arr2len) {
+//         output.push(array2[i])
+//     }
+
+// }
+
+// console.log(output) 
+
+//========================================
+
+
+function reverseInt(int){
+    let intRev = "";
+    let start = int < 0 ? 1 : 0;
+    for (let i=start; i<int.length; i++) {
+        intRev = int[i] + intRev;
+    }
+    return int < 0 ? '-' + intRev : intRev;
+}
+
+console.log("12345 in reverse is:  " + reverseInt("12345"));
+console.log("-12345 in reverse is: " + reverseInt("-12345"));
